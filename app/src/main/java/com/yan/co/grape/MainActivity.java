fd2a30bb6897a2d64a72a05aac1d6f11497e6ddc
smilebin818 <522081447@qq.com>
2016-11-04 18:22:40 +0800

    追加一个首页！

--- app/src/main/java/com/yan/co/grape/MainActivity.java
+++ app/src/main/java/com/yan/co/grape/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // [今日动态]按钮取得
+        final Button dynamic = (Button) findViewById(R.id.dynamic);
+
+        // [今日动态]添加监听器：向【今日动态】画面进行迁移
+        dynamic.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // 迁移动作
+                Log.e("","test");
+//                final TextView text = (TextView) findViewById(R.id.textView1);
+//                text.setText("OnClick. " + " ....");
+            }
+        });
+
     }
 
     @Override
