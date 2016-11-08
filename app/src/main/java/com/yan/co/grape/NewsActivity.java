506160af57837426633e0c686df2312048fbf21d
smilebin818 <522081447@qq.com>
2016-11-08 17:56:05 +0800

    尝试添加ListView控件！

--- app/src/main/java/com/yan/co/grape/NewsActivity.java
+++ app/src/main/java/com/yan/co/grape/NewsActivity.java
@@ -4,6 +4,12 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yanbin on 2016/11/07.
@@ -13,7 +19,41 @@ public class NewsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+
+        SimpleAdapter adapter = new SimpleAdapter(this,getProductInfo(),R.layout.news_main,
+                new String[]{"title","info","img"},
+                new int[]{R.id.title,R.id.info,R.id.imgProduct});
+//        setListAdapter(adapter);
+
+
+
         setContentView(R.layout.news_main);
+    }
+
+    private List<Map<String, Object>> getProductInfo() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "产品①");
+        map.put("info", "产品说明：XXXXXXXXXXXXXXXXXXXXXXXXXX");
+        map.put("img", R.drawable.cute0);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "产品②");
+        map.put("info", "产品说明：YYYYYYYYYYYYYYYYYY");
+        map.put("img", R.drawable.cute0);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "产品③");
+        map.put("info", "产品说明：XXXXXXXXXZZZZZZZZZ");
+        map.put("img", R.drawable.cute0);
+        list.add(map);
+
+        return list;
     }
 
     @Override
