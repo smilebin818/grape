6b21d9faaad929fb03668e4da969180ec70d6e9c
zhanglintc <zhanglintc623@gmail.com>
2016-11-10 22:01:35 +0800

    介绍一个正确变更MainActivity标签名字的姿势

--- app/src/main/java/com/yan/co/grape/MainActivity.java
+++ app/src/main/java/com/yan/co/grape/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.my_app);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
